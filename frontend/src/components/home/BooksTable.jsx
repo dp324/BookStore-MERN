36630c402606a6f47d2e184ae36a9edd128f133e
Deepanshu <deepanshusharma324@gmail.com>
2024-04-01 23:46:08 +0530

    more responsive

--- frontend/src/components/home/BooksTable.jsx
+++ frontend/src/components/home/BooksTable.jsx
@@ -12,8 +12,8 @@ const BooksTable = ({books}) => {
                 <tr>
                     <th className="border border-slate-600 rounded-md">No.</th>
                     <th className="border border-slate-600 rounded-md">Title</th>
-                    <th className="border border-slate-600 rounded-md max-md:hidden">Author</th>
-                    <th className="border border-slate-600 rounded-md max-md:hidden">Publish-Year</th>
+                    <th className="border border-slate-600 rounded-md">Author</th>
+                    <th className="border border-slate-600 rounded-md">Publish-Year</th>
                     <th className="border border-slate-600 rounded-md">Operations</th>
                 </tr>
             </thead>
